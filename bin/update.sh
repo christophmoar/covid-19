7d1ff9d42970ab0b1ebd6adfd0498e57096b3c9c
Christoph Moar <christoph.moar@alpin.it>
2020-04-02 21:44:00 +0200

    new data 21:45

--- bin/update.sh
+++ bin/update.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+cd ../../covid19-bz-scraper && python3 scraper.py && cd -
+cp ../../covid19-bz-scraper/data/covid19_bz.csv ../dataset-01
 cp ../dataset-01/covid19-normalized.CSV ../dataset-01/covid19-normalized-noheader.CSV
 sed -i '1,2d' ../dataset-01/covid19-normalized-noheader.CSV
 /usr/bin/node ./update.js
