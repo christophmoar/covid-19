3eb952e7f46346b30ad36dc9706f74d3f9423d2f
Christoph Moar <christoph.moar@alpin.it>
2020-04-04 10:47:01 +0200

    new datasource for suedtirol detailed data as of 20200403 24:00

--- bin/update.cmd
+++
@@ -1,4 +0,0 @@
-cp ../dataset-01/covid19-normalized.CSV ../dataset-01/covid19-normalized-noheader.CSV
-sed -i '1,2d' ../dataset-01/covid19-normalized-noheader.CSV
-node update.js
-
--- bin/update.sh
+++ bin/update.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-cd ../../covid19-bz-scraper && python3 scraper.py && cd -
-cp ../../covid19-bz-scraper/data/covid19_bz.csv ../dataset-01
+
+## get data from abaumg
+##cd ../../covid19-bz-scraper && python3 scraper.py && cd -
+##cp ../../covid19-bz-scraper/data/covid19_bz.csv ../dataset-01
+
+## get data from simedia
+rm -f ../dataset-01/covid19_bz_detailed.csv
+rm -f ../dataset-01/covid19_bz_detailed.json
+wget -c "http://api.corona-bz.simedia.cloud/?format=json" -O ../dataset-01/covid19_bz_detailed.json
+python3 json_to_csv.py ../dataset-01/covid19_bz_detailed.json ../dataset-01/covid19_bz_detailed.csv
+rm -f ../dataset-01/covid19_bz_detailed.json
+
+## get local data
 cp ../dataset-01/covid19-normalized.CSV ../dataset-01/covid19-normalized-noheader.CSV
 sed -i '1,2d' ../dataset-01/covid19-normalized-noheader.CSV
+
+## update
 /usr/bin/node ./update.js
